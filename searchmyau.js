e7686b0964e36ca1bfd580707baa4f2bdf6717bd
ameyweb <104552969+ameyweb@users.noreply.github.com>
2022-05-28 18:26:27 +0530

    Update searchmyau.js

--- searchmyau.js
+++ searchmyau.js
@@ -31,25 +31,6 @@ $.get(url, function(data){
 
 }
 
-
-if (window.location.protocol == 'http:') {
-     
-    console.log("you are accessing us via "
-        +  "an insecure protocol (HTTP). "
-        + "Redirecting you to HTTPS.");
-         
-    window.location.href =
-        window.location.href.replace(
-                   'http:', 'https:');
-}
-else
-    (window.location.protocol == "https:") {
-        console.log("you are accessing us via"
-            + " our secure HTTPS protocol.");
-    }
-
-
- 
    function enterfun() {
      if(page === "search..."){
      var ev = new KeyboardEvent('keydown', {altKey:false,
